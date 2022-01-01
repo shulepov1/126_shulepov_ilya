f987769b49f44940c8482f89533f7af356f59468
shulepov1 <75673020+shulepov1@users.noreply.github.com>
2022-01-01 14:34:11 +0300

    Added test Create_CreateDocumentsList_ListSizeEqualsZero

--- task_10/src/src/Tests/DocumentTests.java
+++ task_10/src/src/Tests/DocumentTests.java
@@ -2,5 +2,9 @@ package Tests;
 import org.junit.*;
 
 public class DocumentTests extends Assert{
-
+    @Test
+    public void Create_CreateDocumentsList_ListSizeEqualsZero(){
+        DocumentsList documentsList = DocumentsList.create();
+        assertEquals(0, documentsList.getDocumentsCount());
+    }
 }
