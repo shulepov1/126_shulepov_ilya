77bdc3c7545567dd43152f43a62c920266d4f054
shulepov1 <75673020+shulepov1@users.noreply.github.com>
2022-01-01 14:46:07 +0300

    added test addDocument_AddDocumentWithNumberAndDate_ListSizeEqualsOne()

--- task_10/src/src/Tests/DocumentTests.java
+++ task_10/src/src/Tests/DocumentTests.java
@@ -4,8 +4,14 @@ import Core.*;
 
 public class DocumentTests extends Assert{
     @Test
-    public void Create_CreateDocumentsList_ListSizeEqualsZero(){
+    public void create_CreateDocumentsList_ListSizeEqualsZero(){
         DocumentsList documentsList = DocumentsList.create();
         assertEquals(0, documentsList.getDocumentsCount());
+    }
+    @Test
+    public void addDocument_AddDocumentWithNumberAndDate_ListSizeEqualsOne(){
+        DocumentsList documentsList = DocumentsList.create();
+        documentsList.addDocument("number", "date");
+        assertEquals(1, documentsList.getDocumentsCount());
     }
 }
